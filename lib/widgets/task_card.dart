51e106b169962d69f70ca256b9d5b6f16506df8b
arsh <arshiyan.khan13@gmail.com>
2022-02-21 14:37:09 +0530

    read me updated

--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -14,7 +14,7 @@ class _TaskCardState extends State<TaskCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.only(left: 10,right: 10,top:20,bottom: 20),
+        margin: EdgeInsets.only(left: 10,right: 10,top:18,bottom: 18),
         width: double.infinity,
         height: 55,
         decoration: BoxDecoration(
@@ -29,14 +29,51 @@ class _TaskCardState extends State<TaskCard> {
           ]
         ),
         child: TextButton(
-          onPressed: (){},
-          child: Text(
+          onPressed: (){
+            setState(() {
+              widget.task.isCompleted=!widget.task.isCompleted;
+            });
+          },
+          child: Row(
+            children: [
+              Text(
             widget.task.title,
             style: TextStyle(
-              fontSize: 18,
-              color: Colors.grey[600]
-            ),),
+              fontSize: 16,
+              color: widget.task.isCompleted?Colors.grey[400]:Colors.grey[600],
+              decoration: widget.task.isCompleted?TextDecoration.lineThrough:TextDecoration.none,
+            ),
+            ),
+            Spacer(),
+            Container(
+              width: 26,
+              height: 26,
+              child: Icon(
+              Icons.check,
+              color:Colors.white,
+              size: 18
+              ),
+              decoration: BoxDecoration(
+                color: widget.task.isCompleted?Colors.green:Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(100)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(30, 0, 0, 0),
+                    blurRadius: 4,
+                    offset: Offset(0,4)
+                  )
+                ]
+              ),
+            )
+          ],
+          ),
+            style: TextButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(500))
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 20)
         ),
+      ),
     );
   }
 }
